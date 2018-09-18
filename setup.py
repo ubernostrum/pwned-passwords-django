dd203499e1e18b49c6fdc8afa40c46618b53e8a0
James Bennett <james@b-list.org>
2018-09-18 08:32:04 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(name='pwned-passwords-django',
       zip_safe=False,  # eggs are the devil.
-      version='1.2.1',
+      version='1.3',
       description='A Pwned Passwords implementation for Django sites.',
       long_description=open(
           os.path.join(
@@ -23,6 +23,7 @@ setup(name='pwned-passwords-django',
                    'Framework :: Django',
                    'Framework :: Django :: 1.11',
                    'Framework :: Django :: 2.0',
+                   'Framework :: Django :: 2.1',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: BSD License',
                    'Operating System :: OS Independent',
@@ -33,6 +34,7 @@ setup(name='pwned-passwords-django',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
                    'Topic :: Utilities'],
       python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
       install_requires=[
