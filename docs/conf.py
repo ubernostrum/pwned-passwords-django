31fbdd04189de5df2b4defdf26bf997ed6dec886
James Bennett <james@b-list.org>
2024-11-07 23:40:41 -0800

    Don't scan git history in docs spellcheck.

--- docs/conf.py
+++ docs/conf.py
@@ -61,6 +61,10 @@ spelling_lang = "en_US"
 # Location of word list.
 spelling_word_list_filename = "spelling_wordlist.txt"
 
+# The documentation does not include contributor names, so we skip this because it's
+# flaky about needing to scan commit history.
+spelling_ignore_contributor_names = False
+
 # OGP metadata configuration.
 ogp_enable_meta_description = True
 ogp_site_url = "https://pwned-passwords-django.readthedocs.io/"
